c5e8960e326880ffab5fadcff8bd1cb191458235
unknown <abhay.patle04@gmail.com>
2024-06-03 00:43:32 +0530

    third commit

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -12,8 +12,8 @@ const Intro = () => {
             <span className="introText"> I'm <span className="introName"> Abhay </span> <br /> Web Developer </span>
             <p className="introPara">I am a skilled full stack web developer with 1 year of personal experience in creating visually interactive and user-friendly websites. </p>
             <p className="introPara">Persuing my BTech Final Year from Government College of Engineering Amravati in Information Technology</p>
-            <p className="introPara">CGPA-8.78</p>
-            <a href="https://acrobat.adobe.com/id/urn:aaid:sc:AP:a31eda92-1a34-4e55-83fb-d8b72a7f6f6c"><button className="btn"><img src={btnImg} alt="Hire Me" className='btnImg' />Hire Me</button></a>
+            <p className="introPara">CGPA-8.80</p>
+            <a href="https://drive.google.com/file/d/1_CQSJSgdvvjiLMqCH5C33669aRXPdjK_/view?usp=sharing"><button className="btn"><img src={btnImg} alt="Hire Me" className='btnImg' />Hire Me</button></a>
             </div>
             <div className="imageContainer">
         <img src={bg} alt="Profile" className="bg" />
